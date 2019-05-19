009c48b8f9f36e525d42e0d6ccf644f3ef641f6d
SaCut <45201979+SaCut@users.noreply.github.com>
2019-05-19 18:58:18 +0200

    Update and rename FanficScrape_v3.0.py to FanficScrape_v3.1.py
    Added option to try applying reader mode to URL

--- FanficScrape_v3.1.py
+++ FanficScrape_v3.1.py
@@ -17,14 +17,14 @@ def clear_output_folder():
 	    except Exception as e:
 	        print(e)
 
-def get_fic(url, path='Output/', textbox=None, mode=None):								#TODO add reader mode
+def get_fic(url, path='Output/', textbox=None, mode=None):
 	''' Takes a XenForo thread URL and prints each threadmarked section
 	in an html file. '''
 	
 	# Init
 	counter = 1
 	try:
-		thread = Thread(url)
+		thread = Thread(url, mode)
 	except Exception as e:
 		if textbox is not None:
 			update_text(textbox, e)
@@ -35,35 +35,35 @@ def get_fic(url, path='Output/', textbox=None, mode=None):								#TODO add read
 	for i, page in enumerate(thread.pages):
 
 		if i == 0:
-			chapter_title = thread.first_title								# Set first title
+			chapter_title = thread.first_title							# Set first title
 
-			thread.add_headers(path, chapter_title, thread.title)						# Initialise chapter with headers
+			thread.add_headers(path, chapter_title, thread.title)					# Initialise chapter with headers
 
-		sections = thread.slice_page(page)									# Slice webpage in sections (one each post)
+		sections = thread.slice_page(page)								# Slice webpage in sections (one each post)
 		
-		for _, section in enumerate(sections):									# Evaluate each section
+		for _, section in enumerate(sections):								# Evaluate each section
 
-			threadmark = section.find("span", class_="label")						# Find if the post contains a new chapter
+			threadmark = section.find("span", class_="label")					# Find if the post contains a new chapter
 
 			if threadmark is not None:
 				counter += 1
 
-				thread.add_closers(path, chapter_title)							# Wrap up previous file
+				thread.add_closers(path, chapter_title)						# Wrap up previous file
 
-				chapter_title = thread.chapter_title(threadmark, counter)				# Set the name of the next chapter
+				chapter_title = thread.chapter_title(threadmark, counter)			# Set the name of the next chapter
 
-				thread.add_headers(path, chapter_title, thread.title)					# Write new chapter name to file
+				thread.add_headers(path, chapter_title, thread.title)				# Write new chapter name to file
 
-			message = thread.pull_content(section)								# Search for a post in each section
+			message = thread.pull_content(section)							# Search for a post in each section
 			if message is not None:
 
-				with open(path + chapter_title, 'a') as file:						# Write post content to file
+				with open(path + chapter_title, 'a') as file:					# Write post content to file
 					for content in message.contents:
 						file.write(str(content))
 					
-					file.write('<hr noshade="noshade" size="2"/>\n')				# Add a cool separator between posts
+					file.write('<hr noshade="noshade" size="2"/>\n')			# Add a cool separator between posts
 
-	update_text(textbox, '"' + thread.title + '"' + ' finished')
+	update_text(textbox, '"' + thread.title + '"' + ' finished\n\nready')
 
 def get_path(path):
 	''' Updates download path '''
@@ -80,11 +80,12 @@ def main():
 
 	#------ INIT ------
 	# Window
-	window = gui.Window(title='Fanfic Scrape', width=700, height=450, minwidth=500, minheight=350)
+	window = gui.Window(title='Fanfic Scrape', width=700, height=450, minwidth=600, minheight=450)
 	
 	# Misc
 	url = tk.StringVar(value='Insert the address here')
 	path = tk.StringVar(value='Output/')
+	mode = tk.BooleanVar(value=False)
 
 	#----- WINDOW SETUP ------
 	base = tk.Frame(window)
@@ -117,21 +118,31 @@ def main():
 	url_bar = gui.Bar(search, url.get(), textvariable=url)
 	url_bar.pack(expand='yes', fill='x', padx=4)
 	
+	# Options
+	options = tk.Frame(base)
+	options.pack(fill='both', padx=20)
+
+	mode = tk.Checkbutton(options,
+		text="Try to read thread in reader mode (without non-threadmarked posts)",
+		variable=mode, onvalue=True, offvalue=False)
+	mode.pack(side='left', fill='x')
+
+	#TODO add options
 
 	# Text output
 	out_frame = tk.Frame(base)
 	out_frame.pack(side='bottom', fill='x', pady=20, padx=20)
 
 	textframe = tk.Frame(out_frame)
-	textframe.pack(side='bottom', fill='x', pady=20, padx=20)
+	textframe.pack(side='bottom', fill='x', pady=20)
 
-	feedback = tk.Text(textframe, height=4, bg='#708090', fg='turquoise', wrap='word')
+	feedback = tk.Text(textframe, height=5, bg='#708090', fg='turquoise', wrap='word')
 	update_text(feedback, 'ready')
 	feedback.pack(expand='yes', fill='x')
 
 	# Fetch content
 	fetch = tk.Frame(out_frame)
-	fetch.pack(anchor='s', expand='yes', fill='x', padx=20)
+	fetch.pack(anchor='s', expand='yes', fill='x')
 
 	fetch_button = tk.Button(fetch,
 		text='GET',
